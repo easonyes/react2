2a7ecbe0fa4785333a1b1a19c74f43a8adc15571
潘康 <pankang@wahaha.com.cn>
2020-08-18 14:57:43 +0800

    初始化项目

---
+++ fighting/src/Counter.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+class ControlPanel extends Component {
+  // React 新的前端思维方式 •！• 5
+  constructor(props) {
+    super(props);
+    this.onClickButton = this.onClickButton.bind(this);
+    this.state = {
+      count: 0,
+    };
+  }
+  onClickButton() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClickButton}>Click Me</button>
+        <div>Click Count : {this.state.count}</div>
+      </div>
+    );
+  }
+}
+export default ControlPanel;
